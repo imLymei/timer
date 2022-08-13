98ea79e03c911b5956f74995a669df5d295b62cf
Lymei <lymeicontato@gmail.com>
2022-08-13 00:16:37 -0300

    Timer

---
+++ script.js
@@ -0,0 +1,44 @@
+const futureDate = '8 11 2022';
+/*mes dia ano*/ 
+
+const daysel = document.getElementById('days');
+const hoursel = document.getElementById('hours');
+const minutesel = document.getElementById('mins');
+const secondsel = document.getElementById('seconds');
+
+function countdown() {
+    const eventDate = new Date(futureDate); 
+    const currentDate = new Date();
+
+    const xseconds = (eventDate - currentDate) / 1000;
+
+    const seconds = Math.floor(xseconds) %60;
+    const minutes = Math.floor(xseconds /60) %60;
+    const hours = Math.floor(xseconds /3600) %24;
+    const days = Math.floor(xseconds /86400);
+
+    console.log(days, hours, minutes, seconds);
+    
+    daysel.innerHTML = formatTime(days);
+    hoursel.innerHTML = formatTime(hours);
+    minutesel.innerHTML = formatTime(minutes);
+    secondsel.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    if (time < 10 && time > 0) {
+        return '0'+time;
+    } else {
+        if (time < 0) {
+            return 0;
+        } else {
+            return time;
+        }
+    }
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
